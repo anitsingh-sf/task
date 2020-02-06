f7f0c8546eae6317611ad419fa4d6328fc9f3de2
Anit Singh <anit.singh@sourcefuse.com>
2020-02-06 18:50:51 +0530

    Changed

--- task.js
+++ task.js
@@ -3,9 +3,33 @@ let bodyBeforeEditable;
 
 let deleted = [], initLength;
 let userData = [
-    { "firstName":"John", "middleName":"Smith", "lastName":"Doe", "eMail":"john@sf.com", "phone":"84561", "roll":"Engineer", "address":"Chandigarh" },
-    { "firstName":"Anna", "middleName":"Smith", "lastName":"John", "eMail":"anna@sf.com", "phone":"78965", "roll":"QA", "address":"Mohali" },
-    { "firstName":"Tink", "middleName":"Tong", "lastName":"Tang", "eMail":"ting@sf.com", "phone":"83478", "roll":"DevOps", "address":"Panchkula" }
+    { 
+        "firstName":"John", 
+        "middleName":"Smith",
+        "lastName":"Doe", 
+        "eMail":"john@sf.com", 
+        "phone":"84561", 
+        "roll":"Engineer", 
+        "address":"Chandigarh" 
+    },
+    { 
+        "firstName":"Anna", 
+        "middleName":"Smith", 
+        "lastName":"John", 
+        "eMail":"anna@sf.com", 
+        "phone":"78965", 
+        "roll":"QA", 
+        "address":"Mohali" 
+    },
+    { 
+        "firstName":"Tink", 
+        "middleName":"Tong", 
+        "lastName":"Tang", 
+        "eMail":"ting@sf.com", 
+        "phone":"83478", 
+        "roll":"DevOps", 
+        "address":"Panchkula" 
+    }
 ];
 
 function showData() {
@@ -80,8 +104,8 @@ function editData(num) {
             ele[index].setAttribute("contenteditable", "false");
         } 
         changeButton(num);
-    }
-    
+    }   
+
 }
 
 function changeButton(num) {
